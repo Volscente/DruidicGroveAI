70636b7e59e6b7c0e94b6faa851ee48f6c7284ef
Simone Porreca <porrecasimone@gmail.com>
2024-10-12 23:53:10 +0200

    Create bigquery_connector.py

---
+++ src/bigquery_connector/bigquery_connector.py
@@ -0,0 +1,14 @@
+# TODO Docstrings
+class BigqueryConnector:
+    # TODO Docstring
+    def __init__(self):
+        # TODO
+        pass
+
+    def _get_client(self):
+        # TODO
+        pass
+
+    def read_from_query_config(self):
+        # TODO
+        pass
