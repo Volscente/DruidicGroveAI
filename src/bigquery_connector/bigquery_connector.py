99f83a33f0fc1b84b51e7afdec78aae10257d24e
Simone Porreca <porrecasimone@gmail.com>
2024-10-13 12:13:00 +0200

    Init BigqueryConnector

--- src/bigquery_connector/bigquery_connector.py
+++ src/bigquery_connector/bigquery_connector.py
@@ -1,9 +1,38 @@
-# TODO Docstrings
+"""
+Defines the BigqueryConnector class in order to query BigQuery
+datasets and tables
+"""
+# Import Standard Modules
+from pathlib import Path
+
+# Import Package Modules
+from src.logging_module.logging_module import get_logger
+
+
 class BigqueryConnector:
-    # TODO Docstring
+    """
+    The class implements a BigQuery Connector
+    in order to query BigQuery datasets and tables
+
+    Attributes:
+
+    Methods:
+
+    """
     def __init__(self):
-        # TODO
-        pass
+        """
+        Constructor of the class BigqueryConnector
+
+        Args:
+
+        """
+        # Setup logger
+        self.logger = get_logger(__class__.__name__,
+                                 pathlib.Path(__file__).parents[1] /
+                                 'logging_module' /
+                                 'log_configuration.yaml')
+
+        self.logger.debug('__init__ - Initialise object attributes')
 
     def _get_client(self):
         # TODO
