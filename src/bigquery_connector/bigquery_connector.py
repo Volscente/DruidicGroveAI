f54eea96e947dd9ed6c75da0fb4556d09d80f328
Simone Porreca <porrecasimone@gmail.com>
2024-10-14 12:26:45 +0200

    Add _get_client

--- src/bigquery_connector/bigquery_connector.py
+++ src/bigquery_connector/bigquery_connector.py
@@ -3,23 +3,27 @@ Defines the BigqueryConnector class in order to query BigQuery
 datasets and tables
 """
 # Import Standard Modules
+from google.cloud import bigquery
 from pathlib import Path
 
 # Import Package Modules
 from src.logging_module.logging_module import get_logger
+from src.types import BigQueryClientConfig
 
 
-class BigqueryConnector:
+class BigQueryConnector:
     """
     The class implements a BigQuery Connector
     in order to query BigQuery datasets and tables
 
     Attributes:
+        client_config: BigQueryClientConfig including all necessary variables for instance a BigQuery Client instance
 
     Methods:
 
     """
-    def __init__(self):
+    def __init__(self,
+                 client_config: BigQueryClientConfig):
         """
         Constructor of the class BigqueryConnector
 
@@ -34,9 +38,32 @@ class BigqueryConnector:
 
         self.logger.debug('__init__ - Initialise object attributes')
 
-    def _get_client(self):
-        # TODO
-        pass
+        # Initialise attributes
+        self.client_config = client_config
+
+        # Set the client
+        _set_client()
+
+    def _set_client(self):
+        """
+        Set the attribute 'client' with an instance of the BigQuery Client
+
+        Returns:
+            client: bigquery.Client object
+        """
+        self.logger.debug('_set_client - Start')
+
+        self.logger.info('_set_client - Set the BigQuery client')
+
+        # Set the client
+        self.client = bigquery.Client(
+            project=self.client_config.project_id,
+            location=self.client_config.location
+        )
+
+        self.logger.debug('_set_client - End')
+
+
 
     def read_from_query_config(self):
         # TODO
