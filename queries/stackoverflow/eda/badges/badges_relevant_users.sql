c5cb72409e859103a9691b36e4d8f21b86ee144b
Simone Porreca <porrecasimone@gmail.com>
2024-11-06 21:54:53 +0100

    Update badges_relevant_users.sql

--- queries/stackoverflow/eda/badges/badges_relevant_users.sql
+++ queries/stackoverflow/eda/badges/badges_relevant_users.sql
@@ -18,8 +18,19 @@ _badges AS (
 -- Retrieve badge information of relevant users
 _badges_relevant_users AS (
     SELECT
+        users.*,
+        badges.name AS badge_name,
+        badges.date AS badge_date,
+        badges.class AS badge_class,
+        badges.tag_based AS badge_tag_based
     FROM
         _badges as badges
-    
+    INNER JOIN _relevant_users AS users
+        ON badges.user_id = users.id
 )
+
+-- Select all the badge information retrieve
+# TODO: Investigate count of _relevant_users and _badges_relevant_users
+SELECT *
+FROM _badges_relevant_users
 
