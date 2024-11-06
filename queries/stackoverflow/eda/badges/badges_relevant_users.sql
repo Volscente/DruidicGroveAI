d2d067e9ead108581889662231891e83bcfd61d0
Simone Porreca <porrecasimone@gmail.com>
2024-11-06 21:51:45 +0100

    Update badges_relevant_users.sql

--- queries/stackoverflow/eda/badges/badges_relevant_users.sql
+++ queries/stackoverflow/eda/badges/badges_relevant_users.sql
@@ -1,36 +1,25 @@
 /*
- * Retrieve badge information of the users who
- * have not null values in their information
- * and who recently performed their last access into the platform
+ * Retrieve badge information of the relevant users
 */
--- Compute the latest access datetime
-WITH _max_last_access_date AS (
-    SELECT MAX(stackoverflow_user.last_access_date) AS max_last_access_date
+-- Retrieve relevant users
+WITH _relevant_users AS (
+    SELECT *
     FROM
-        `bigquery-public-data.stackoverflow.users` AS stackoverflow_user
-),
-
--- Select only users for which the last access is maximum 8 hours greater than the max_last_access_date
-_most_recent_last_access_users AS (
-    SELECT stackoverflow_user.*
-    FROM
-        `bigquery-public-data.stackoverflow.users` AS stackoverflow_user,
-        _max_last_access_date
-    WHERE
-        stackoverflow_user.last_access_date BETWEEN TIMESTAMP(DATE_SUB(_max_last_access_date.max_last_access_date, INTERVAL @hours_interval HOUR))
-        AND _max_last_access_date.max_last_access_date
-),
-
--- Select only users which have all the information filled
--- NOTE: age is rarely filled
-_relevant_users AS (
-    SELECT relevant_user.*
-    FROM
-        _most_recent_last_access_users AS relevant_user
-    WHERE
-        relevant_user.display_name IS NOT NULL
-      AND relevant_user.location IS NOT NULL
+        `deep-learning-438509.dim_stackoverflow_data_model.relevant_users`
 ),
 
 -- Retrieve badge information
+_badges AS (
+    SELECT *
+    FROM
+        `bigquery-public-data.stackoverflow.badges`
+),
+
+-- Retrieve badge information of relevant users
+_badges_relevant_users AS (
+    SELECT
+    FROM
+        _badges as badges
+    
+)
 
