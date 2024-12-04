6913ae7b1d99eb6cfe2d414b22abe7ec73342313
Simone Porreca <porrecasimone@gmail.com>
2024-12-04 18:51:54 +0100

    Update post_answers_relevant_users.sql

--- queries/stackoverflow/eda/posts_answers/post_answers_relevant_users.sql
+++ queries/stackoverflow/eda/posts_answers/post_answers_relevant_users.sql
@@ -26,8 +26,32 @@ _latest_posts_answers AS (
 
 -- Filter for relevant users and add users information
 _relevant_users_answers AS (
-    -- TODO
+    SELECT *
+    FROM
+        _latest_posts_answers AS posts_answers
+    JOIN _relevant_users AS users
+        ON posts_answers.owner_user_id = users.id
 )
 
 -- Select only relevant information
--- TODO
+SELECT
+    posts_answers.title AS answer_title,
+    posts_answers.body AS answer_body,
+    posts_answers.answer_count AS answer_count,
+    posts_answers.comment_count AS answer_comment_count,
+    posts_answers.creation_date AS answer_creation_date,
+    posts_answers.favorite_count AS answer_favorite_count,
+    posts_answers.view_count AS answer_view_count,
+    posts_answers.tags AS answer_tags,
+    posts_answers.display_name AS user_display_name,
+    posts_answers.about_me AS user_about_me,
+    posts_answers.creation_date_1 AS user_creation_date,
+    posts_answers.last_access_date AS user_last_access_date,
+    posts_answers.location AS user_location,
+    posts_answers.reputation AS user_reputation,
+    posts_answers.up_votes AS user_up_votes,
+    posts_answers.down_votes AS user_down_votes,
+    posts_answers.views AS user_views,
+    posts_answers.score AS answer_score,
+FROM
+    _relevant_users_answers AS posts_answers
