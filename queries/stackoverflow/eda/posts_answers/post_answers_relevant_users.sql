a573ee73360925d310e3829292cd23dc62ad7804
Simone Porreca <porrecasimone@gmail.com>
2024-12-04 19:23:16 +0100

    Refactor post answers query

--- queries/stackoverflow/eda/posts_answers/post_answers_relevant_users.sql
+++ queries/stackoverflow/eda/posts_answers/post_answers_relevant_users.sql
@@ -1,6 +1,6 @@
 /*
  * Retrieve post answers of the relevant users in the last time frame from the latest post answer's creation date
- * NOTE: The latest post answer's creation date is 25/09/2022 (Checked on 04/12/2024
+ * NOTE: The latest post answer's creation date is 25/09/2022 (Checked on 04/12/2024)
  * NOTE: 'owner_user_id' is the 'id' column of the user who posted the answer in the relevant_users table
  */
 -- Retrieve relevant users
@@ -17,25 +17,14 @@ _latest_posts_answers AS (
         `bigquery-public-data.stackoverflow.posts_answers` AS posts_answers
     WHERE
         -- Filter for time frame
-        posts_answers.creation_date BETWEEN TIMESTAMP(DATE_SUB(DATE(2022, 11, 25), INTERVAL 4 MONTH)) AND TIMESTAMP(CURRENT_DATE())
+        posts_answers.creation_date BETWEEN TIMESTAMP(DATE_SUB(DATE(2022, 11, 25), INTERVAL @months_interval MONTH)) AND TIMESTAMP(CURRENT_DATE())
         -- Filter for posts answers with required information
         AND posts_answers.body IS NOT NULL
         AND posts_answers.owner_user_id IS NOT NULL
         AND posts_answers.score IS NOT NULL
-),
-
--- Filter for relevant users and add users information
-_relevant_users_answers AS (
-    SELECT
-        posts_answers.*,
-        users.*
-    FROM
-        _latest_posts_answers AS posts_answers
-    INNER JOIN _relevant_users AS users
-        ON posts_answers.owner_user_id = users.id
 )
 
--- Select only relevant information
+-- Filter for relevant users and add users information
 SELECT
     posts_answers.title AS answer_title,
     posts_answers.body AS answer_body,
@@ -45,15 +34,17 @@ SELECT
     posts_answers.favorite_count AS answer_favorite_count,
     posts_answers.view_count AS answer_view_count,
     posts_answers.tags AS answer_tags,
-    posts_answers.display_name AS user_display_name,
-    posts_answers.about_me AS user_about_me,
-    posts_answers.creation_date_1 AS user_creation_date,
-    posts_answers.last_access_date AS user_last_access_date,
-    posts_answers.location AS user_location,
-    posts_answers.reputation AS user_reputation,
-    posts_answers.up_votes AS user_up_votes,
-    posts_answers.down_votes AS user_down_votes,
-    posts_answers.views AS user_views,
+    users.display_name AS user_display_name,
+    users.about_me AS user_about_me,
+    users.creation_date AS user_creation_date,
+    users.last_access_date AS user_last_access_date,
+    users.location AS user_location,
+    users.reputation AS user_reputation,
+    users.up_votes AS user_up_votes,
+    users.down_votes AS user_down_votes,
+    users.views AS user_views,
     posts_answers.score AS answer_score
 FROM
-    _relevant_users_answers AS posts_answers
+    _latest_posts_answers AS posts_answers
+INNER JOIN _relevant_users AS users
+    ON posts_answers.owner_user_id = users.id
