b036b1e0dfa9cf22f65e630351660a27993847fc
Simone Porreca <porrecasimone@gmail.com>
2025-01-24 14:58:20 +0100

    Update post_answers.sql

--- queries/stackoverflow/data_preparation/post_answers.sql
+++ queries/stackoverflow/data_preparation/post_answers.sql
@@ -13,16 +13,20 @@ WITH _users AS (
     SELECT user_id
     FROM
         `deep-learning-438509.curated_stackoverflow_data_model.users_information`
-)
+),
 
 -- Retrieve post answers for the relevant users
-SELECT
-    posts_answers.owner_user_id AS answer_user_id,
-    posts_answers.body AS answer_body,
-    posts_answers.comment_count AS answer_comment_count,
-    posts_answers.creation_date AS answer_creation_date,
-    posts_answers.score AS answer_score
-FROM
-    `bigquery-public-data.stackoverflow.posts_answers` AS posts_answers
-WHERE
-    posts_answers.owner_user_id IN (SELECT users.user_id FROM _users AS users)
+_post_aswers AS (
+    SELECT
+        posts_answers.owner_user_id AS answer_user_id,
+        posts_answers.body AS answer_body,
+        posts_answers.comment_count AS answer_comment_count,
+        posts_answers.creation_date AS answer_creation_date,
+        posts_answers.score AS answer_score
+    FROM
+        `bigquery-public-data.stackoverflow.posts_answers` AS posts_answers
+    WHERE
+        posts_answers.owner_user_id IN (SELECT users.user_id FROM _users AS users)
+)
+
+-- Filter just for the last @
