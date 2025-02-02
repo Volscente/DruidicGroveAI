3650514ef30fd97b9ca69962375f7351b8926e94
Simone Porreca <porrecasimone@gmail.com>
2025-02-02 13:04:43 +0100

    Create test_raw_dataset.sql

---
+++ queries/test_queries/stackoverflow_data_preparation/test_raw_dataset.sql
@@ -0,0 +1,67 @@
+/*
+ * Create a test table for the Raw Dataset by joining the following test input tables for the StackOverflow use case:
+ * 1. `deep-learning-438509.dim_stackoverflow_data_model.test_users_information`
+ * 2. `deep-learning-438509.dim_stackoverflow_data_model.test_top_rarest_badges`
+ * 3. `deep-learning-438509.dim_stackoverflow_data_model.test_post_answers`
+ */
+CREATE OR REPLACE TABLE `deep-learning-438509.dim_stackoverflow_data_model.test_raw_dataset` AS
+-- Retrieve users information
+WITH _users AS (
+    SELECT *
+    FROM
+        `deep-learning-438509.dim_stackoverflow_data_model.test_users_information`
+),
+
+-- Retrieve top rarest badges
+_badges AS (
+    SELECT *
+    FROM
+        `deep-learning-438509.dim_stackoverflow_data_model.test_top_rarest_badges`
+),
+
+-- Retrieve post answers
+_posts AS (
+    SELECT *
+    FROM
+        `deep-learning-438509.dim_stackoverflow_data_model.test_post_answers`
+),
+
+-- Define the column has_most_rare_badges
+_users_badges AS (
+    SELECT
+      users.*,
+      CASE
+        WHEN EXISTS (
+          SELECT 1
+          FROM UNNEST(users.badges_list) AS badge
+          WHERE badge IN (SELECT badge_name FROM _badges)
+        ) THEN 1
+        ELSE 0
+      END AS has_most_rare_badges
+    FROM
+      _users AS users
+)
+
+-- Join with post answers
+SELECT
+    users.user_name,
+    users.user_about_me,
+    users.user_creation_date,
+    users.user_location,
+    users.user_reputation,
+    users.user_up_votes,
+    users.user_down_votes,
+    users.user_views,
+    users.user_profile_image_url,
+    users.user_website_url,
+    users.badge_count,
+    users.badges_list,
+    users.has_most_rare_badges,
+    posts.answer_body,
+    posts.answer_comment_count,
+    posts.answer_creation_date,
+    posts.answer_score
+FROM
+    _users_badges AS users
+LEFT JOIN _posts AS posts
+    ON users.user_id = posts.answer_user_id
