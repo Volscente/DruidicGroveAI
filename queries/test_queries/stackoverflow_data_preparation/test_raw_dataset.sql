574f81f435b8ba43a8d89dfb898fb08936537f4a
Simone Porreca <porrecasimone@gmail.com>
2025-02-03 13:51:45 +0100

    Refactor raw_dataset queries

--- queries/test_queries/stackoverflow_data_preparation/test_raw_dataset.sql
+++ queries/test_queries/stackoverflow_data_preparation/test_raw_dataset.sql
@@ -30,39 +30,51 @@ _posts AS (
 -- Define the column has_most_rare_badges
 _users_badges AS (
     SELECT
-      users.*,
-      CASE
-        WHEN EXISTS (
-          SELECT 1
-          FROM UNNEST(users.badges_list) AS badge
-          WHERE badge IN (SELECT badge_name FROM _badges)
-        ) THEN 1
-        ELSE 0
-      END AS has_most_rare_badges
+        users.*,
+        COALESCE(
+            MAX(CASE WHEN badges.badge_name IS NOT NULL THEN 1 ELSE 0 END),
+            0
+        ) AS has_most_rare_badges
     FROM
-      _users AS users
-)
+        _users AS users
+    LEFT JOIN UNNEST(users.badges_list) AS user_badge
+    LEFT JOIN _badges AS badges
+        ON user_badge = badges.badge_name
+    -- Ensure to put together users with multiple badges
+    GROUP BY ALL
+),
 
 -- Join with post answers
+_users_with_post_answers AS (
+    SELECT
+        users.user_name,
+        users.user_about_me,
+        users.user_creation_date,
+        users.user_location,
+        users.user_reputation,
+        users.user_up_votes,
+        users.user_down_votes,
+        users.user_views,
+        users.user_profile_image_url,
+        users.user_website_url,
+        users.badge_count,
+        users.badges_list,
+        users.has_most_rare_badges,
+        posts.answer_body,
+        posts.answer_comment_count,
+        posts.answer_creation_date,
+        posts.answer_score
+    FROM
+        _users_badges AS users
+    LEFT JOIN _posts AS posts
+        ON _users_badges.user_id = posts.answer_user_id
+)
+
+-- Filter for users without post answers
 SELECT
-    users.user_name,
-    users.user_about_me,
-    users.user_creation_date,
-    users.user_location,
-    users.user_reputation,
-    users.user_up_votes,
-    users.user_down_votes,
-    users.user_views,
-    users.user_profile_image_url,
-    users.user_website_url,
-    users.badge_count,
-    users.badges_list,
-    users.has_most_rare_badges,
-    posts.answer_body,
-    posts.answer_comment_count,
-    posts.answer_creation_date,
-    posts.answer_score
+    *
 FROM
-    _users_badges AS users
-LEFT JOIN _posts AS posts
-    ON users.user_id = posts.answer_user_id
+    _users_with_post_answers
+WHERE
+    answer_body IS NOT NULL
+
