342cd840e364c46786f238d909127ac7298e8a1e
Simone Porreca <porrecasimone@gmail.com>
2025-09-08 18:13:12 +0200

    Logger init

--- src/postgresql_connector/postgresql_connector.py
+++ src/postgresql_connector/postgresql_connector.py
@@ -2,7 +2,14 @@
 The module includes the PostgreSQL connector to interact
 with the PostgreSQL database.
 """
-#
+
+# Import Standard Libraries
+import os
+from pathlib import Path
+
+# Import Package Modules
+from src.logging_module.logging_module import get_logger
+from src.postgresql_connector.postgresql_types import PostgreSQLClientConfig
 
 
 class PostgreSQLConnector:
@@ -14,5 +21,24 @@ class PostgreSQLConnector:
 
     """
 
-    def __init__(self):
+    def __init__(self, client_config: PostgreSQLClientConfig):
+        """
+        Constructor of the class PostgreSQLConnector
+
+        Args:
+            client_config (PostgreSQLClientConfig): Config for instance a PostgreSQL Client
+        """
+        # Setup logger
+        self._logger = get_logger(
+            __class__.__name__,
+            Path(os.getenv("DRUIDIC_GROVE_AI_ROOT_PATH"))
+            / "src"
+            / "logging_module"
+            / "log_configuration.yaml",
+        )
+
+        # Initialise attributes
+        self._client_config = client_config
+
+    def _set_client(self):
         pass
