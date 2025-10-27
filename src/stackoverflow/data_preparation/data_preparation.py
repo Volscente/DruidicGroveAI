2df787e5cf2c5f2197a93cbb38c78daf0dc19d10
Simone Porreca <porrecasimone@gmail.com>
2025-10-27 16:56:56 +0100

    Add module data_preparation

---
+++ src/stackoverflow/data_preparation/data_preparation.py
@@ -0,0 +1,17 @@
+"""
+The module includes classes for implementing data preparation
+steps for the StackOverflow use cases.
+"""
+
+# Import Standard Modules
+import logging
+
+# Setup logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+
+
+class AnswerClassification:
+    pass
