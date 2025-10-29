c6c61b46cdeeff19f2ff7ef4a054f5ca9d457b00
Simone Porreca <porrecasimone@gmail.com>
2025-10-29 18:42:06 +0100

    Init AnswerScoreRawDataFlow

---
+++ src/stackoverflow/pipelines/data/answer_score_raw_data_flow.py
@@ -0,0 +1,8 @@
+"""
+The module implements a Metaflow Pipeline for creating the Raw Data Layer for the StackOverflow
+Answer Score Classification use case.
+"""
+
+
+class AnswerScoreRawDataFlow:
+    pass
